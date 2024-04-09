ab7d0f9713dff11a9b25e35e814bd333f85dde0a
Enzo Volkmann <enzo@volkmann.dev>
2024-04-09 15:44:58 +0200

    implement `getRandomFloatsInRange` without unpredictable while loop

--- src/RandomHelpers.php
+++ src/RandomHelpers.php
@@ -2,17 +2,22 @@
 
 namespace VolkmannDesignCode\KirbyUtils;
 
+use Exception;
+
 class RandomHelpers {
     static function getRandomFloatBetween(float $min, float $max): float
     {
         return rand($min * 10, $max * 10) / 10;
     }
 
+    /**
+     * @throws Exception if the minimum difference does not fit at least twice into the range between `$min` and `$max`
+     */
     static function getRandomFloatsInRange(float $min, float $max, int $count, float $minDiff = 0): array
     {
         $range = $max - $min;
         if (($range - $minDiff) < $minDiff) {
-            throw new \Exception('$minDiff should fit at least twice into the range between $min and $max. Otherwise a timeout is very likely.');
+            throw new Exception('$minDiff should fit at least twice into the range between $min and $max');
         }
 
         $results = [];
@@ -20,8 +25,22 @@ class RandomHelpers {
             $candidate = self::getRandomFloatBetween($min, $max);
 
             // Ensure the minimum difference to the previous number
-            while ($i > 0 && abs($results[$i - 1] - $candidate) < $minDiff) {
-                $candidate = self::getRandomFloatBetween($min, $max);
+            if ($i > 0 && $minDiff > 0) {
+                $diff = $results[$i - 1] - $candidate;
+                $sign = $diff < 0 ? -1 : 1;
+
+                if (abs($diff < $minDiff)) {
+                    $missing = abs($diff) - $minDiff;
+                    $candidate += $sign * $missing;
+                }
+
+                // Perform corrections if the candidate is out of bounds
+                if ($candidate < $min) {
+                    $candidate = self::getRandomFloatBetween($max - $minDiff, $max);
+                } else if ($candidate > $max) {
+                    $candidate = self::getRandomFloatBetween($min, $min + $minDiff);
+                }
+
             }
 
             $results[] = $candidate;
--- tests/RandomHelpersTest.php
+++ tests/RandomHelpersTest.php
@@ -24,4 +24,25 @@ final class RandomHelpersTest extends TestCase
         }
     }
 
+    public function testGetRandomFloatsBetweenMinDiff(): void
+    {
+        $min = 10.0;
+        $max = 20.0;
+        $count = 100;
+        $minDiff = 2.0;
+        $res = RandomHelpers::getRandomFloatsInRange($min, $max, $count, $minDiff);
+
+        $prev = 0;
+        foreach ($res as $i => $r) {
+            if ($i > 0) {
+                $diff = abs($prev - $r);
+                $this->assertTrue($r >= $min && $r <= $max, $r . ' is not in range' . $min . ' - ' . $max);
+                // Make a good enough comparison of the floats.
+                $this->assertTrue($minDiff - $diff * 1.0001 < 0, 'diff ' . $diff . ' is not >= ' . $minDiff);
+            }
+            $prev = $r;
+        }
+
+    }
+
 }
