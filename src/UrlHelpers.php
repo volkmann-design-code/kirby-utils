adbed4b239705670633890dd6ff15b08e3a0ea24
Enzo Volkmann <enzo@volkmann.dev>
2025-09-13 10:17:28 +0200

    feat: add external link helpers

--- src/UrlHelpers.php
+++ src/UrlHelpers.php
@@ -2,6 +2,9 @@
 
 namespace VolkmannDesignCode\KirbyUtils;
 
+use Kirby\Content\Field;
+use Kirby\Toolkit\Str;
+
 class UrlHelpers {
 
     static function telLink(string $phoneNumber): string
@@ -12,6 +15,23 @@ class UrlHelpers {
     static function mailtoLink(string $email): string
     {
         return 'mailto:' . $email;
+    }
+
+    static function linkTarget(Field $linkField): string
+    {
+        return self::isExternalLink($linkField) ? '_blank' : '_self';
+    }
+
+    static function linkRel(Field $linkField, string $externalRel = 'nofollow noopener noreferrer'): string
+    {
+        return self::isExternalLink($linkField) ? $externalRel : '';
+    }
+
+    static function isExternalLink(Field $linkField): bool
+    {
+        $langCode = $kirby->language()?->code() ?? '';
+        $siteRoot = $site->url()->beforeEnd("/$langCode");
+        return Str::startsWith($linkField, 'http://') && !Str::startsWith($linkField, $siteRoot);
     }
 
 }
